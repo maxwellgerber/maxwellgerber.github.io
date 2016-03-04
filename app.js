5959687672be3a0a61d1f595d86b0d6e57fab8c1
Maxwell Gerber <mgerber@berkeley.edu>
2016-03-04 14:33:35 -0800

    better redirect for 404

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs    = require("fs");
 
 var app = express();
 app.set('view engine', 'jade');
@@ -10,10 +11,20 @@ app.get('/', function (request, response) {
   response.render('index');
 });
 
+app.get('/404', function (request, response) {
+  response.send('404 error lol');
+});
+
 app.get('/:stuff', function (request, response) {
-	console.log(request.params.stuff + ' requested')
-	var parsed = request.params.stuff.replace('\.html','')
-  response.render(parsed);
+	console.log(request.params.stuff + ' requested');
+	var parsed = request.params.stuff.replace('\.html','');
+  response.render(parsed, {}, function(err, html){
+  	if(err){
+  		response.redirect('/404');
+  	} else {
+  		response.send(html);
+  	}
+  });
 });
 
 app.listen(80);
