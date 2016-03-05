162397150715a7ea87a67e4cc8e25c6bc7e2d105
Maxwell Gerber <mgerber@berkeley.edu>
2016-03-04 17:12:44 -0800

    got jade templates working

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var fs    = require("fs");
+var nodemailer = require('nodemailer');
+
 
 var app = express();
 app.set('view engine', 'jade');
@@ -20,10 +22,40 @@ app.get('/:stuff', function (request, response) {
 	var parsed = request.params.stuff.replace('\.html','');
   response.render(parsed, {}, function(err, html){
   	if(err){
-  		response.redirect('/404');
+  		response.render('404');
   	} else {
   		response.send(html);
   	}
+  });
+});
+
+app.post('/contact', function (req, res) {
+  var mailOpts, smtpTrans;
+  console.log(req);
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport('SMTP', {
+      service: 'Gmail',
+      auth: {
+          user: "maxwell.gerber42@gmail.com",
+          pass: "Josephine1!" 
+      }
+  });
+  //Mail options
+  mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
+      to: 'mgerber@berkeley.edu',
+      subject: 'Website contact form',
+      text: req.body.message
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
+      }
+      //Yay!! Email sent
+      else {
+          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+      }
   });
 });
 
