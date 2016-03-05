4423b2968961de0ecdce3d49c44badc3ad77adfe
Maxwell Gerber <mgerber@berkeley.edu>
2016-03-04 17:42:59 -0800

    404 page done

--- app.js
+++ app.js
@@ -29,34 +29,5 @@ app.get('/:stuff', function (request, response) {
   });
 });
 
-app.post('/contact', function (req, res) {
-  var mailOpts, smtpTrans;
-  console.log(req);
-  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
-  smtpTrans = nodemailer.createTransport('SMTP', {
-      service: 'Gmail',
-      auth: {
-          user: "maxwell.gerber42@gmail.com",
-          pass: "Josephine1!" 
-      }
-  });
-  //Mail options
-  mailOpts = {
-      from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
-      to: 'mgerber@berkeley.edu',
-      subject: 'Website contact form',
-      text: req.body.message
-  };
-  smtpTrans.sendMail(mailOpts, function (error, response) {
-      //Email not sent
-      if (error) {
-          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
-      }
-      //Yay!! Email sent
-      else {
-          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
-      }
-  });
-});
 
 app.listen(80);
