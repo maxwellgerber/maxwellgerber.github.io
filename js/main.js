b92e2a5f2043098592e807c3176922df831393c1
Maxwell Gerber <mgerber@berkeley.edu>
2018-01-14 21:16:02 -0500

    minor updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("Should do something cool here");
+  console.log(
+  	"You found the console! Easter eggs coming in soon.",
+  	"In an effort to reduce page load as much as possible I've decided to not use jQuery.",
+  	"Have fun with this site!"
+  	);
 });
