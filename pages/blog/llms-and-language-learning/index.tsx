a6f5fd9e1aebe58eda03fe0f2e74e747ae3c9a3d
Maxwell Gerber <max@stytch.com>
2025-05-18 10:25:06 -0700

    fix build

--- pages/blog/llms-and-language-learning/index.tsx
+++
@@ -1,6 +0,0 @@
-import Content from "./content.mdx";
-import BlogLayout from "@/components/BlogLayout";
-
-export default function Page() {
-    return <BlogLayout slug="llms-and-language-learning"><Content/></BlogLayout>;
-}
