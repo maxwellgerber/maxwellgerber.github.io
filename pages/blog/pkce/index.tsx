eb7b4df47bf270bab4ea28f4f1ebc5f55406af7d
Maxwell Gerber <max@stytch.com>
2025-05-17 19:14:37 -0700

    SEO bits

--- pages/blog/pkce/index.tsx
+++ pages/blog/pkce/index.tsx
@@ -1,12 +1,23 @@
 import Content from "./content.mdx";
 import {BlogFooter} from "@/components/BlogFooter";
+import Head from "next/head";
 
 export default function Page() {
     return (
-        <div className="container">
-            <Content/>
-            <BlogFooter/>
-        </div>
+        <>
+            <Head>
+                <title>Why PKCE Is My Favorite OAuth Standard</title>
+                <meta name="description" content="PKCE is the best OAuth standard."/>
+                <meta name="robots" content="index, follow"/>
+                <link rel="canonical" href="https://maxgerber.com/blog/pkce"/>
+            </Head>
+            <div className="container">
+                <article>
+                    <Content/>
+                </article>
+                <BlogFooter/>
+            </div>
+        </>
     )
 
 }
